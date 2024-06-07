5da004b486e3b9b007b12280de698bebb3bc3216
MariusLeChapelier <mariuslechapelier@gmail.com>
2024-06-07 16:54:27 +0200

    interruption working but a lot of improvements to do

---
+++ speaker_interruption.py
@@ -0,0 +1,171 @@
+"""
+SpeakerModule_Interruption
+==================
+
+This module outputs the audio signal contained in the AudioIUs by the computer's speakers,
+and can interrupt this audio streaming if the user starts speaking (with the reception of VADStateIUs).
+"""
+
+import datetime
+import threading
+import time
+import pyaudio
+import retico_core
+import platform
+import retico_core.abstract
+from retico_core.audio import AudioIU, SpeakerModule
+from utils import *
+
+from utils import *
+from vad import VADStateIU
+
+
+class SpeakerModule_Interruption(retico_core.AbstractConsumingModule):
+    """A module that consumes AudioIUs of arbitrary size and outputs them to the
+    speakers of the machine. It stops outputting audio if the user starts speaking,
+    because it also receives information trough the VADStateIU.
+    When a new IU is incoming, the module blocks as long as the current IU is being played.
+    """
+
+    @staticmethod
+    def name():
+        return "Speaker Interruption Module"
+
+    @staticmethod
+    def description():
+        return "A consuming module that plays audio from speakers and stops playing audio if the user starts speaking (it receives a VADStateIU == True)"
+
+    @staticmethod
+    def input_ius():
+        return [AudioIU, VADStateIU]
+
+    @staticmethod
+    def output_iu():
+        return None
+
+    def __init__(
+        self,
+        rate=44100,
+        frame_length=0.2,
+        sample_width=2,
+        channels=1,
+        use_speaker="both",
+        device_index=None,
+        log_file="speaker_interruption.csv",
+        log_folder="logs/test/16k/Recording (1)/demo",
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.rate = rate
+        self.sample_width = sample_width
+        self.use_speaker = use_speaker
+
+        self._p = pyaudio.PyAudio()
+
+        if device_index is None:
+            device_index = self._p.get_default_output_device_info()["index"]
+        self.device_index = device_index
+
+        self.stream = None
+        self.time = None
+
+        self.channels = channels
+        self.log_file = manage_log_folder(log_folder, log_file)
+        self.time_logs_buffer = []
+        self.first_time = True
+
+        # interruptions parameters
+        self.vad_state = False
+        self.audio_buffer = []
+        self.old_iu = None
+        self.frame_length = frame_length
+
+    def process_update(self, update_message):
+        """overrides SpeakerModule : https://github.com/retico-team/retico-core/blob/main/retico_core/audio.py#L282
+
+        overrides SpeakerModule's process_update to save logs.
+        """
+        # TODO: replace this method by an actual way of knowing what is the starting and ending time where the speaker is active (actually outputs time, and not receive messages).
+        if self.first_time:
+            self.time_logs_buffer.append(
+                ["Start", datetime.datetime.now().strftime("%T.%f")[:-3]]
+            )
+            self.first_time = False
+        else:
+            self.time_logs_buffer.append(
+                ["Stop", datetime.datetime.now().strftime("%T.%f")[:-3]]
+            )
+        for iu, ut in update_message:
+            if isinstance(iu, VADStateIU):
+                if ut == retico_core.UpdateType.ADD:
+                    if iu.payload and not self.vad_state:
+                        print("Someone starts talking, speakers stops playing audio")
+                    elif not iu.payload and self.vad_state:
+                        print("user stoped talking")
+                    self.vad_state = iu.payload
+            elif isinstance(iu, AudioIU):
+                if ut == retico_core.UpdateType.ADD:
+                    if not self.vad_state and self.old_iu != iu.previous_iu:
+                        self.audio_buffer.append(bytes(iu.raw_audio))
+                    else:
+                        self.old_iu = iu
+            else:
+                # raise Exception("Unknown IU type " + iu)
+                # raise NotImplementedError("Unknown IU type " + iu)
+                raise TypeError("Unknown IU type " + iu)
+
+        return None
+
+    def callback(self, in_data, frame_count, time_info, status):
+        if len(self.audio_buffer) == 0:
+            silence_bytes = b"\x00" * frame_count * self.channels * self.sample_width
+            return (silence_bytes, pyaudio.paContinue)
+        data = self.audio_buffer.pop()
+        return (data, pyaudio.paContinue)
+
+    def setup(self):
+        """overrides SpeakerModule : https://github.com/retico-team/retico-core/blob/main/retico_core/audio.py#L288"""
+        return
+
+    def prepare_run(self):
+        """overrides SpeakerModule : https://github.com/retico-team/retico-core/blob/main/retico_core/audio.py#L288"""
+
+        p = self._p
+
+        if platform.system() == "Darwin":
+            if self.use_speaker == "left":
+                stream_info = pyaudio.PaMacCoreStreamInfo(channel_map=(0, -1))
+            elif self.use_speaker == "right":
+                stream_info = pyaudio.PaMacCoreStreamInfo(channel_map=(-1, 0))
+            else:
+                stream_info = pyaudio.PaMacCoreStreamInfo(channel_map=(0, 0))
+        else:
+            stream_info = None
+
+        # Adding the stream_callback parameter should make the stream.write() function non blocking,
+        # which would make it possible to run in parallel of the reception of update messages (and the uptdate of vad_state)
+        self.stream = p.open(
+            format=p.get_format_from_width(self.sample_width),
+            channels=self.channels,
+            rate=self.rate,
+            input=False,
+            output_host_api_specific_stream_info=stream_info,
+            output=True,
+            output_device_index=self.device_index,
+            stream_callback=self.callback,
+            frames_per_buffer=int(self.rate * self.frame_length),
+        )
+
+        self.stream.start_stream()
+
+    def shutdown(self):
+        """overrides SpeakerModule : https://github.com/retico-team/retico-core/blob/main/retico_core/audio.py#L312
+
+        Write logs and close the audio stream."""
+        write_logs(
+            self.log_file,
+            self.time_logs_buffer,
+        )
+        self.stream.stop_stream()
+        self.stream.close()
+        self.stream = None
---
+++ vad.py
@@ -0,0 +1,143 @@
+"""
+VAD Module
+==================
+
+This module provides Voice Activity Detection (VAD) using a webrtcvad.
+"""
+
+import retico_core
+from retico_core.audio import AudioIU
+import pydub
+import webrtcvad
+
+from utils import *
+
+
+class VADStateIU(retico_core.abstract.IncrementalUnit):
+    """An Incremental IU describing the vad state (if someone is talking or not).
+
+    Attributes:
+        creator (AbstractModule): The module that created this IU
+        previous_iu (IncrementalUnit): A link to the IU created before the
+            current one.
+        grounded_in (IncrementalUnit): A link to the IU this IU is based on.
+        created_at (float): The UNIX timestamp of the moment the IU is created.
+        vad_state (bool): The vad state.
+    """
+
+    @staticmethod
+    def type():
+        return "VAD State IU"
+
+    def __init__(
+        self,
+        creator=None,
+        iuid=0,
+        previous_iu=None,
+        grounded_in=None,
+        vad_state=None,
+        **kwargs,
+    ):
+        super().__init__(
+            creator=creator,
+            iuid=iuid,
+            previous_iu=previous_iu,
+            grounded_in=grounded_in,
+            payload=vad_state,
+        )
+        self.vad_state = vad_state
+
+    def set_vad_state(self, vad_state):
+        """Sets the vad_state"""
+        self.payload = vad_state
+        self.vad_state = vad_state
+
+
+class VADModule(retico_core.AbstractModule):
+    """A retico module that provides Voice Activity Detection (VAD) using a webrtcvad.
+
+    Inputs : AudioIU
+
+    Outputs : VADStateIU
+    """
+
+    @staticmethod
+    def name():
+        return "webrtcvad Module"
+
+    @staticmethod
+    def description():
+        return "A module that recognizes if someone is talking using webrtcvad."
+
+    @staticmethod
+    def input_ius():
+        return [AudioIU]
+
+    @staticmethod
+    def output_iu():
+        return VADStateIU
+
+    def __init__(
+        self,
+        printing=False,
+        log_file="vad.csv",
+        log_folder="logs/test/16k/Recording (1)/demo",
+        vad_aggressiveness=3,
+        target_framerate=16000,
+        input_framerate=44100,
+        channels=1,
+        sample_width=2,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.target_framerate = target_framerate
+        self.input_framerate = input_framerate
+        self.channels = channels
+        self.sample_width = sample_width
+        self.vad = webrtcvad.Vad(vad_aggressiveness)
+        self.printing = printing
+        self.log_file = manage_log_folder(log_folder, log_file)
+        self.time_logs_buffer = []
+
+    def resample_audio(self, audio):
+        """Resample the audio's frame_rate to correspond to self.target_framerate.
+
+        Args:
+            audio (bytes): the audio received from the microphone that could need resampling.
+
+        Returns:
+            bytes: the resampled audio chunk.
+        """
+        if self.input_framerate != self.target_framerate:
+            s = pydub.AudioSegment(
+                audio,
+                sample_width=self.sample_width,
+                channels=self.channels,
+                frame_rate=self.input_framerate,
+            )
+            s = s.set_frame_rate(self.target_framerate)
+            return s._data
+        return audio
+
+    def process_update(self, update_message):
+        """
+        overrides AbstractModule : https://github.com/retico-team/retico-core/blob/main/retico_core/abstract.py#L402
+
+        Args:
+            update_message (UpdateType): UpdateMessage that contains new IUs, if the IUs are ADD,
+            they are added to the audio_buffer.
+        """
+        for iu, ut in update_message:
+            if ut != retico_core.UpdateType.ADD:
+                continue
+            if self.input_framerate != iu.rate:
+                raise Exception("input framerate differs from iu framerate")
+            output_iu = self.create_iu(iu)
+            output_iu.set_vad_state(
+                self.vad.is_speech(
+                    self.resample_audio(iu.raw_audio), self.target_framerate
+                )
+            )
+            return retico_core.UpdateMessage.from_iu(
+                output_iu, retico_core.UpdateType.ADD
+            )
